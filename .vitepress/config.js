3f09570efe4c4dcd5600fd7e8964e970298478e1
mvezina <mvezina@cegepgarneau.ca>
2023-09-01 15:11:08 -0400

    fix ExempleBanque.png + add bloc-note.xml

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -37,7 +37,8 @@ export default defineConfig({
         text: 'En vrac',
         items: [
           { text: 'Convention de nommage', link: '/convention' },
-          { text: 'UtilEnum.cs', link: 'https://gitlab.com/420-14b-fx/contenu/-/raw/main/en_vrac/UtilEnum.cs?ref_type=heads&inline=false'}
+          { text: 'UtilEnum.cs', link: 'https://gitlab.com/420-14b-fx/contenu/-/raw/main/en_vrac/UtilEnum.cs?ref_type=heads&inline=false'},
+          { text: 'Bloc-note.xml', link: 'https://gitlab.com/420-14b-fx/contenu/-/raw/main/en_vrac/Bloc-notes.xml?ref_type=heads&inline=false' },
         ]
       },
       /*{
